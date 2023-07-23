723199eac9690b328066ec1ea27ccd78d5b7945a
einaromar <kiritoruizeo@gmail.com>
2023-07-23 02:58:16 -0600

    5.2.2 Update shopping cart section, add product to cart, fix display of product details

--- src/views/Shop/Carrito.jsx
+++ src/views/Shop/Carrito.jsx
@@ -1,20 +1,45 @@
-// Carrito.js
-import React, { useContext } from 'react';
+
+import React, { useEffect, useState, useContext } from 'react';
 import Box from '@mui/material/Box';
 import Badge from '@mui/material/Badge';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import MailIcon from '@mui/icons-material/Mail';
-import { CarritoContext } from '../../context/CarritoContext';
 import { ShoppingCartRounded } from '@mui/icons-material';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Paper, useTheme } from '@mui/material';
+import { doc, getDoc, updateDoc, setDoc, collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../../config/firebase/firebaseDB";
+import { CarritoContext } from '../../context/CarritoContext';
+import { useAuth } from "../../context/AuthContext";
 
 const Carrito = () => {
   const { contador, open, abrirDialog, cerrarDialog } = useContext(CarritoContext);
   const theme = useTheme();
+  const { user } = useAuth();
+  const [sumaNumeros, setSumaNumeros] = useState(contador);
 
+  // const obtenerDatosCarritoYSumar = async () => {
+  //   if (user) {
+  //     const referencia = doc(db, `carritoUsuario/${user.uid}`);
+  //     const docSnap = await getDoc(referencia);
+  //     const data = docSnap.exists() ? docSnap.data() : null;
+  //     setSumaNumeros(data ? data.total : 0);
+  //   }
+  // };
+  const obtenerDatosCarritoYSumar = async () => {
+    if (user) {
+      const referencia = doc(db, `carritoUsuario/${user.uid}`);
+      // Utilizar onSnapshot para escuchar cambios en el documento del carrito del usuario
+      onSnapshot(referencia, (docSnap) => {
+        const data = docSnap.exists() ? docSnap.data() : null;
+        setSumaNumeros(data ? data.total : 0);
+        console.log("Se recibieron datos actualizados del carrito:", data);
+      });
+     
+    }
+  };
+
+  useEffect(() => {
+    obtenerDatosCarritoYSumar();
+  }, [user]);
   return (
     <>
       <Box
@@ -24,7 +49,6 @@ const Carrito = () => {
           position: "fixed",
           right: -15,
           zIndex: 1,
-
           color: 'action.active',
           display: 'flex',
           flexDirection: 'column',
@@ -36,23 +60,13 @@ const Carrito = () => {
           },
         }}
       >
-        <Paper elevation={0} sx={{p:1, border: `3px solid ${theme.palette.primary.main}`, borderRightColor: "transparent"}}>
-          <Badge color="secondary" badgeContent={contador}>
+        <Paper elevation={0} sx={{ p: 1, border: `3px solid ${theme.palette.primary.main}`, borderRightColor: "transparent" }}>
+          <Badge color="secondary" badgeContent={sumaNumeros}>
             <ShoppingCartRounded />
           </Badge>
-          {/* <ButtonGroup>
-            <Button aria-label="reduce" onClick={disminuirContador}>
-            <RemoveIcon fontSize="small" />
-            </Button>
-            <Button aria-label="increase" onClick={aumentarContador}>
-            <AddIcon fontSize="small" />
-            </Button>
-          </ButtonGroup> */}
         </Paper>
       </Box>
 
-
-      
       <Dialog
         fullWidth
         disableScrollLock
@@ -65,15 +79,10 @@ const Carrito = () => {
           Carrito de compras
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Item 1
+          <DialogContentText>
+            Total: {sumaNumeros}
           </DialogContentText>
-          <DialogContentText id="alert-dialog-description">
-            Item 2
-          </DialogContentText>
-          <DialogContentText id="alert-dialog-description">
-            Item 3
-          </DialogContentText>
+          {/* Aquí puedes mostrar los productos del carrito si lo deseas */}
         </DialogContent>
         <DialogActions>
           <Button variant='outlined' onClick={cerrarDialog}>Cerrar</Button>
--- src/views/Shop/DetalleProduct.jsx
+++ src/views/Shop/DetalleProduct.jsx
@@ -1,209 +1,244 @@
-import { Avatar, Box, Button, Chip, Divider, Grid, ListItem, ListItemAvatar, ListItemText, Paper, Typography } from '@mui/material'
-import { Stack } from '@mui/system'
-import { ItemListCard } from '../../components/customs/ItemListCard'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Avatar, Box, Button, Chip, Divider, Grid, ListItem, ListItemAvatar, ListItemText, Paper, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+import { ItemListCard } from '../../components/customs/ItemListCard';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { WrapperSingleRoute } from '../../components/customs/WrapperSingleRoute';
 import { Bread } from '../../components/customs/Bread';
-import { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import { HomeRounded, StoreRounded, LocalOfferRounded } from '@mui/icons-material';
-import Carrito from './Carrito';
+import Carrito  from './Carrito';
 import { CarritoContext } from '../../context/CarritoContext';
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { doc, getDoc, updateDoc, setDoc } from "firebase/firestore";
+import { db } from "../../config/firebase/firebaseDB";
 
+export const DetalleProduct = ({ productos }) => {
+  const [datos, setDatos] = useState(null);
 
+  const params = useParams();
+  const { user } = useAuth();
+  const navigate = useNavigate();
 
-export const DetalleProduct = ({productos}) => {
-    const { aumentarContador } = useContext(CarritoContext);
+  useEffect(() => {
+    // Verificar si los datos de 'productos' están disponibles y no están vacíos antes de configurar 'datos'
+    if (productos && productos.length > 0) {
+      console.log('Productos:', productos);
+      let p = productos.filter((producto) => producto.id === params.id);
+      console.log('Datos filtrados:', p);
+      setDatos(p[0].data());
+      console.log('Datos:', p[0].data());
+    }
+    //eslint-disable-next-line
+  }, [productos]);
 
-    const [datos, setDatos] = useState("");
-    
-    const params = useParams();
-
-    useEffect(() => {
-        let p = productos.filter(producto => producto.id === params.id);
-        setDatos(p[0].data()); 
-        //eslint-disable-next-line 
-    }, []);  
-    
+  
+  const registrarProducto = async (idProduct) => {
+    if (!user) {
+      return navigate("/acceso");
+    }
+  
+    // Obtener la referencia del documento del carrito del usuario
+    const referencia = doc(db, `carritoUsuario/${user.uid}`);
+  
+    // Obtener los datos del carrito del usuario
+    const docSnap = await getDoc(referencia);
+    const data = docSnap.exists() ? docSnap.data() : null;
+  
+    // Si el carrito no existe, crearlo con el producto y cantidad 1
+    if (!data) {
+      await setDoc(referencia, { [params.id]: 1, total: 1 });
+    } else {
+      // Si el producto ya existe en el carrito, aumentar la cantidad
+      if (data.hasOwnProperty(params.id)) {
+        await updateDoc(referencia, {
+          [params.id]: data[params.id] + 1,
+          total: data.total ? data.total + 1 : 1, // Sumar 1 al total si ya existe, o crearlo con 1 si aún no existe
+        });
+      } else {
+        // Si el producto no existe en el carrito, agregarlo con cantidad 1
+        await updateDoc(referencia, {
+          [params.id]: 1,
+          total: data.total ? data.total + 1 : 1, // Sumar 1 al total si ya existe, o crearlo con 1 si aún no existe
+        });
+      }
+    }
+  };
+  
+  
+  // Conditional rendering to handle the case where 'datos' is not available yet
+  if (!datos) {
+    return <div>Loading...</div>; // You can also show a loading spinner here.
+  }
 
   return (
-    <Box sx={{bgcolor: "background.default"}}>
-        {/* BREADCUMBS */}
-        <Grid 
-            container 
-            rowSpacing={1} 
-            columnSpacing={{xs: 1, sm: 3, md: 5}}
-        >
-            <Grid item xs > 
-                <Bread migas={[{miga: "INICIO", ruta: "/inicio", icono: <HomeRounded/>},{miga: "TIENDA", ruta: "/tienda", icono: <StoreRounded/>},{miga: `${datos.nombre}`, ruta: `/tienda/${params.id}`, icono: <LocalOfferRounded/>}]}/>
-                <Carrito/>
-            </Grid>
+    <Box sx={{ bgcolor: "background.default" }}>
+      {/* BREADCUMBS */}
+      <Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 3, md: 5 }}
+      >
+        <Grid item xs >
+          <Bread migas={[
+            { miga: "INICIO", ruta: "/inicio", icono: <HomeRounded /> },
+            { miga: "TIENDA", ruta: "/tienda", icono: <StoreRounded /> },
+            { miga: `${datos.nombre}`, ruta: `/tienda/${params.id}`, icono: <LocalOfferRounded /> }
+          ]} />
+          <Carrito />
         </Grid>
-        
-        {/* CONTENIDO */}
-        {/* Imagen principal */}{/* Precio-botones-descBreve */}
-        <Grid container spacing={3}>
-            <Grid item xs={12} md={7}>
-                <ItemListCard 
-                    img={datos.url}
-                    heightImg={500}
-                    showContent={false}
+      </Grid>
+
+      {/* CONTENIDO */}
+      {/* Imagen principal */}{/* Precio-botones-descBreve */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={7}>
+          <ItemListCard
+            img={datos.url}
+            heightImg={500}
+            showContent={false}
+          />
+
+          <Grid container rowSpacing={1} columnSpacing={1}>
+            <Grid item xs={6} md={4} >
+              <ItemListCard
+                img={datos.url}
+                heightImg={150}
+                showContent={false}
+              />
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <ItemListCard
+                img={datos.url}
+                heightImg={150}
+                showContent={false}
+              />
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <ItemListCard
+                img={datos.url}
+                heightImg={150}
+                showContent={false}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <Paper sx={{ p: 1, m: 2 }}>
+            <Typography variant='h3' textAlign="center" component="p" >
+              {datos.nombre}
+            </Typography>
+            <Typography variant='h5' textAlign="center" component="p" fontWeight="10" p={3}>
+              ${datos.costo} MXN
+            </Typography>
+            <Typography variant='body1' textAlign="center" component="p" fontWeight="10">
+              {datos.descripcion}
+            </Typography>
+            <Box textAlign="center" p={3}>
+              <Typography fontWeight="bold" textAlign="left">Categoria(s):</Typography>
+              <Chip label={datos.categoria} color="primary" variant="outlined" sx={{ mr: 1 }} />
+            </Box>
+            <Divider variant='inset' light />
+            <Box textAlign="left" p={3}>
+              <Stack spacing={2} direction="row">
+                <Button variant="contained" startIcon={<AttachMoneyIcon />}>Buy</Button>
+                <Button variant="outlined" startIcon={<ShoppingCartIcon />} onClick={() => registrarProducto()}>Add car</Button>
+              </Stack>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      {/* TESTIMONIOS */}
+      <Grid container>
+        <Grid item xs>
+          <Typography variant='h5' textAlign="center" component="p" fontWeight="50" p={1}>
+            Testimonios
+          </Typography>
+          <Box display="flex" justifyContent="center" >
+            <ListItem alignItems="center" sx={{ width: "70%" }}>
+              <ListItemAvatar>
+                <Avatar alt="Cindy Baker" sx={{ bgcolor: "primary.main" }}>R</Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={<Typography variant='h6' component="p">Great product</Typography>}
+                secondary={
+                  <>
+                    <Typography
+                      sx={{ display: 'inline' }}
+                      component="span"
+                      variant="body1"
+                      color="text.primary"
+                    >
+                      Remy Sharp
+                    </Typography>
+                    {' —  Despite seeing no many bad reviews on them coming in broken , I took the risk and happy I did. I got the 8 piece and they were all in perfect condition. Thanks!'}
+                  </>
+                }
+              />
+            </ListItem>
+          </Box>
+        </Grid>
+      </Grid>
+
+      {/* DESCRIPCION LARGA */}
+      <Grid container >
+        <Grid item xs>
+          <Paper sx={{ ml: 5, mr: 5, p: 3 }}>
+            <Typography variant='h5' textAlign="left" component="p" fontWeight="bold" p={1}>
+              Descripcion
+            </Typography>
+            <Typography variant='body1' textAlign="left" component="p" fontWeight="10">
+              {/* Long description content */}
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+
+      {/* PRODUCTOS RELACIONADOS */}
+      <Grid container >
+        <Grid item xs>
+          <Paper sx={{ ml: 5, mr: 5, p: 3, bgcolor: 'background.default' }} elevation={2}>
+            <Typography variant='h5' textAlign="left" component="p" fontWeight="bold" p={1}>
+              Productos relacionados
+            </Typography>
+
+            <Grid container spacing={0}>
+              <Grid item xs={12} sm={6} md={4}>
+                <ItemListCard
+                  img={datos.url}
+                  heightImg={250}
+                  showContent={false}
                 />
-                
-                <Grid container rowSpacing={1} columnSpacing={1}>
-                    <Grid item xs={6} md={4} >
-                        <ItemListCard 
-                            img={datos.url}
-                            heightImg={150}
-                            showContent={false}
-                        />
-                    </Grid>
-                    <Grid item xs={6} md={4}>
-                        <ItemListCard 
-                            img={datos.url}
-                            heightImg={150}
-                            showContent={false}
-                        />
-                    </Grid>
-                    <Grid item xs={6} md={4}>
-                        <ItemListCard 
-                            img={datos.url}
-                            heightImg={150}
-                            showContent={false}
-                        />
-                    </Grid>
-                </Grid>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <ItemListCard
+                  img={datos.url}
+                  heightImg={250}
+                  showContent={false}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <ItemListCard
+                  img={datos.url}
+                  heightImg={250}
+                  showContent={false}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <ItemListCard
+                  img={datos.url}
+                  heightImg={250}
+                  showContent={false}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={5}>
-                <Paper sx={{p:1, m:2}}>
-                    <Typography variant='h3' textAlign="center" component="p" >
-                        {datos.nombre}
-                    </Typography>
-                    <Typography variant='h5' textAlign="center" component="p" fontWeight="10" p={3}>
-                        ${datos.costo} MXN
-                    </Typography>
-                    <Typography variant='body1' textAlign="center" component="p" fontWeight="10">
-                        {datos.descripcion}
-                    </Typography>
-                    <Box textAlign="center" p={3}>
-                        <Typography fontWeight="bold" textAlign="left">Categoria(s):</Typography>
-                        <Chip label={datos.categoria} color="primary" variant="outlined" sx={{mr:1}} />
-                    </Box>
-                    <Divider variant='inset' light />
-                    <Box textAlign="left" p={3}>
-                        <Stack spacing={2} direction="row">
-                            <Button variant="contained" startIcon={<AttachMoneyIcon/>}>Buy</Button>
-                            <Button variant="outlined" startIcon={<ShoppingCartIcon/>} onClick={aumentarContador}>Add car</Button>
-                        </Stack>
-                    </Box>
-                </Paper>
-            </Grid>
+          </Paper>
         </Grid>
+      </Grid>
 
-        {/* TESTIMONIOS */}
-        <Grid container>
-            <Grid item xs>
-                <Typography variant='h5' textAlign="center" component="p" fontWeight="50" p={1}>
-                    Testimonios
-                </Typography>
-                <Box display="flex" justifyContent="center" > 
-                    <ListItem alignItems="center" sx={{width: "70%"}}>
-                        <ListItemAvatar>
-                        <Avatar alt="Cindy Baker" sx={{bgcolor: "primary.main"}}>R</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                        primary={<Typography variant='h6' component="p">Great product</Typography>}
-                        secondary={
-                            <>
-                                <Typography
-                                    sx={{ display: 'inline' }}
-                                    component="span"
-                                    variant="body1"
-                                    color="text.primary"
-                                    >
-                                    Remy Sharp
-                                </Typography>
-                                {' —  Despite seeing no many bad reviews on them coming in broken , I took the risk and happy I did. I got the 8 piece and they were all in perfect condition. Thanks!'}
-                            </>
-                        }
-                        />
-                    </ListItem>
-                </Box>
-            </Grid>
-        </Grid>
-
-        {/* DESCRIPCION LARGA */}
-        <Grid container >
-            <Grid item xs>
-                <Paper sx={{ml:5, mr:5, p:3}}>
-                    <Typography variant='h5' textAlign="left" component="p" fontWeight="bold" p={1}>
-                        Descripcion
-                    </Typography>
-                    <Typography variant='body1' textAlign="left" component="p" fontWeight="10">
-                        <Typography>
-                            {">"} Diseño clásico y eficiente, esta taza hogareña adopta la tradición mexicana de beber de cerámica de arcilla o Barro. La arcilla roja se recoge de las colinas circundantes. Las brocas de madera, piedra y maíz se utilizan como herramientas para moldear la arcilla en una rueda de banda tradicional.
-                        </Typography>
-                        <Typography>
-                            {">"} Esta taza está hecha perfecta para disfrutar de bebidas calientes como champurrado, chocolate caliente, café y mucho más. También puedes mantener tus bebidas frías y frías, ya que funciona tan bien para mantener las bebidas y líquidos fríos como para mantenerlos calientes.
-                        </Typography>
-                        <Typography>
-                            {">"} Como lo utilices, disfruta de tu bebida favorita de elección en esta hermosa taza pintada a mano y hecha a mano. La forma especial y auténtica de la mano del Barro los hace únicos en proporción... un elemento que hace que la mano sea tan hermosamente perfecta.
-                        </Typography>
-                        <Typography>
-                            {">"} Este "Taza", como se conoce en español, es perfecto para bebidas frías y calientes, ya que está hecho para preservar el calor o el frío por igual. Excelente para cualquier ocasión, desde tomar un café caliente por la mañana hasta despertar hasta beber una taza fría de agua en los días más calurosos. No hay roturas, pero ten en cuenta que este material de arcilla está manejado a mano, forma y hecho; el mínimo astillado es natural.
-                        </Typography>
-                        <Typography>
-                            {">"} Esta taza decorativa nunca dejará de destacar con su brillante acristalamiento Barro y cuello ingeniosamente redondeado. Es una gran pieza que también adornará tu cocina, dado un auténtico y tradicional estilo mexicano. Esta taza será una maravillosa adición a cualquier colección de cocina o cerámica mexicana, pero también es una fina decoración. Este artículo incluye 1 taza de barro y mide con una apertura de 3 x 3.5 pulgadas de alto x 5 pulgadas de profundidad. Capacidad para 10 onzas.
-                        </Typography>
-                    </Typography>
-                </Paper>
-            </Grid>
-        </Grid>
-
-        {/* PRODUCTOS RELACIONADOS */}
-        <Grid container >
-            <Grid item xs>
-                <Paper sx={{ml:5, mr:5, p:3, bgcolor: 'background.default'}} elevation={2}>
-                    <Typography variant='h5' textAlign="left" component="p" fontWeight="bold" p={1}>
-                        Productos relacionados
-                    </Typography>
-
-                    <Grid container spacing={0}>
-                        <Grid item xs={12} sm={6} md={4}>
-                            <ItemListCard 
-                                img={datos.url}
-                                heightImg={250}
-                                showContent={false}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={4}>
-                            <ItemListCard 
-                                img={datos.url}
-                                heightImg={250}
-                                showContent={false}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={4}>
-                            <ItemListCard 
-                                img={datos.url}
-                                heightImg={250}
-                                showContent={false}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={4}>
-                            <ItemListCard 
-                                img={datos.url}
-                                heightImg={250}
-                                showContent={false}
-                            />
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Grid>
-        </Grid>
-            
-        {/* FIN DEL CONTENIDO QUE SERÁ EL DEFINITIVO */}
+      {/* FIN DEL CONTENIDO QUE SERÁ EL DEFINITIVO */}
     </Box>
   )
 }
