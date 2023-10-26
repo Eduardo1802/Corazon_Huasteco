b9e823470a7d9a56c2785975be78349e69088560
Eduardo1802 <20200744@uthh.edu.mx>
2023-10-26 03:21:20 -0600

    6.11.17 Test access

--- tests/access.js
+++ tests/access.js
@@ -5,7 +5,7 @@ fixture`Testiando el acceso`.page("https://corazon-huasteco.com/acceso");
 
 test('Validar el acceso', async (t) =>{
     const button = await Selector('.MuiButton-contained.MuiButton-containedPrimary.MuiButton-fullWidth').withText('ENTRAR');;
-    const title = await Selector('.MuiTypography-root.MuiTypography-h6.MuiTypography-gutterBottom.css-adrpgk').withText('Hola, Bienvenido');
+    const title = await Selector('.MuiTypography-root.MuiTypography-h5.css-adrpgk').withText('Hola, Bienvenido');
 
     await t
     .typeText('[name="email"]',"eduazuara0@gmail.com")
