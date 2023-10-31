f88839e66a8d1d730475ede8c57c81705f3310ed
Einar Omar <Kiritoruizeo@gmail.com>
2023-10-31 12:57:00 -0600

    6.18.38 update testcafe

--- tests/test_renderizado_sobre_nosotros.js
+++ tests/test_renderizado_sobre_nosotros.js
@@ -10,5 +10,9 @@ test('Debe mostrar el título "Sobre nosotros"', async (t) => {
 
 test('Debe mostrar el equipo de desarrolladores', async (t) => {
     const teamMembers = Selector('.AvatarItem');
+    
+    // Esperar 5 segundos para que los elementos se carguen
+    await t.wait(5000);
+
     await t.expect(teamMembers.count).eql(5); 
 });
