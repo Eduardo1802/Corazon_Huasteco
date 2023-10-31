193c6feabf0cef251dbe752290d1bf62e7e95971
Eduardo1802 <20200744@uthh.edu.mx>
2023-10-30 23:59:45 -0600

    6.18.34 Test validate email with TestCafe

--- tests/validateEmail.js
+++ tests/validateEmail.js
@@ -5,7 +5,7 @@ fixture`Testiando el registro`.page("https://corazon-huasteco.com/registro");
 test('Validar el registro', async (t) => {
     const btnValidar = await Selector('.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-fullWidth[aria-label="Validar correo electronico"]');
     const alert = await Selector('.MuiAlert-message.css-1xsto0d');
-    
+
     await t
         .typeText('[name="name"]', "Eduardo")
         .typeText('[name="lastName"]', "Azuara")
@@ -14,7 +14,7 @@ test('Validar el registro', async (t) => {
         .typeText('[name="ocupation"]', "Estudiante")
         .typeText('[name="zipCode"]', "43000")
         .typeText('[name="state"]', "Hidalgo")
-        .typeText('[name="email"]', "eduardo1802gmail.com")
+        .typeText('[name="email"]', "eduazuara0@gmail.com")
         .click(btnValidar)
         .expect(alert.innerText).contains("Tu correo es real, procede con el registro");
 });
