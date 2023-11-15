2b870886746c5b4e607eef19afba5d8641ead299
elder <20200710@uthh.edu.mx>
2023-11-15 17:23:45 -0600

    6.22.5 Remove Unnecessary Code Footer (Improve Performance)

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { forwardRef }                                from 'react'
-import { Box, CardMedia, Container, Grid, MenuItem, Snackbar, TextField, Typography, useTheme }  from '@mui/material';
+import { Box, CardMedia, Container, Grid, Snackbar, TextField, Typography, useTheme }  from '@mui/material';
 import MuiAlert from '@mui/material/Alert';
 import { LinkStyled }                       from './footer.elements'
 import imgLogo1                             from "../../../assets/img/app/imgLogoHuejutlaLight.webp"
@@ -9,41 +9,24 @@ import youtube                              from "react-useanimations/lib/youtub
 import instagram                            from "react-useanimations/lib/instagram";
 import { Link }                             from 'react-router-dom';
 
-function Copyright() {
-    return (
-      <>
-        <Box sx={{display: "flex", justifyContent: "center"}}>
+const LANGUAGES = [
+  {
+    id: 1,
+    code: 'es-ES',
+    name: 'Español',
+  },
+  {
+    id: 2,
+    code: 'en-US',
+    name: 'Inglés',
+  },
+];
   
-          <Link color="primary.light" to="/home">
-          {'© '}
-            Corazón Huasteco {" "}
-          {new Date().getFullYear()}
-          </Link>{' '}
-        </Box>
-      </>
-    );
-  }
-  
-  const LANGUAGES = [
-    {
-      id: 1,
-      code: 'es-ES',
-      name: 'Español',
-    },
-    {
-      id: 2,
-      code: 'en-US',
-      name: 'Inglés',
-    },
-    
-  ];
-  
-  const Alert = forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export const Footer = () => {
-
   const theme = useTheme();
 
   const [open, setOpen] = React.useState(false);
@@ -56,89 +39,88 @@ export const Footer = () => {
     if (reason === 'clickaway') {
       return;
     }
-
     setOpen(false);
   };
 
   return (
     <Box bgcolor="primary.main" pt={4}>
-        {/* ALERTA */}
-        <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} anchorOrigin={{ vertical: "top", horizontal: "center"}} aria-label='alerta de error, esta opción no está disponible todavía'>
-            <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
-                Oops, esta opción no está disponible todavía
-            </Alert>
-        </Snackbar>
+      {/* ALERTA */}
+      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} anchorOrigin={{ vertical: "top", horizontal: "center"}} aria-label='alerta de error, esta opción no está disponible todavía'>
+        <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
+          Oops, esta opción no está disponible todavía
+        </Alert>
+      </Snackbar>
 
-        <Container>
-            <Grid container spacing={2} pb={3}>
-                {/* REDES */}
-                <Grid item xs={12} sm={4} md={3} order={{xs: 6, sm: 1, md: 1}} container alignItems="center">
-                    <Grid container direction="column" justifyContent="center" spacing={2}>
-                        <Grid item container direction="row" justifyContent={{xs: "center", sm: "flex-start", md: "flex-start"}} wrap='nowrap'>
-                            <Link to='https://es-la.facebook.com/' target='_BLANK' >
-                                <UseAnimations
-                                    animation={facebook}
-                                    size={56} 
-                                    strokeColor={theme.palette.background.default} 
-                                />
-                            </Link>
-                            <Link to='https://www.instagram.com/' target='_BLANK'>
-                                <UseAnimations
-                                    animation={instagram}
-                                    size={56} 
-                                    strokeColor={theme.palette.background.default} 
-                                />
-                            </Link>
-                            <Link to='https://youtube.com/' target='_BLANK' >
-                                <UseAnimations
-                                    animation={youtube}
-                                    size={56} 
-                                    strokeColor={theme.palette.background.default} 
-                                />
-                            </Link>
-                        </Grid>
-                        <Grid item container justifyContent={{xs: "center", sm: "flex-start", md: "flex-start"}}>
-                            <Typography variant="body1" sx={{color: theme.palette.background.paper}}>
-                                &#169; Corazón Huasteco 2023
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                {/* ENLACES */}
-                <Grid item xs={12} sm={4} md={3} order={{xs: 1, sm: 3, md: 2}} container direction="column" alignItems="flex-start">
-                    <Typography variant="h6" component="p" sx={{ color: theme.palette.background.paper }}>
-                        Categorias
-                    </Typography>
-                    <LinkStyled to="tematicas/vestimenta">Vestimenta</LinkStyled>
-                    <LinkStyled to="tematicas/danza">Danza</LinkStyled>
-                    <LinkStyled to="tematicas/gastronomia">Gastronomia</LinkStyled>
-                    <LinkStyled to="tematicas/musica">Música</LinkStyled>
-                    <LinkStyled to="tematicas/tradiciones">Tradiciones</LinkStyled>
-                </Grid>
-                <Grid item xs={12} sm={4} md={3} order={{xs: 2, sm: 4, md: 3}} container direction="column" alignItems="flex-start">
-                    <Typography variant="h6" component="p" sx={{ color: theme.palette.background.paper }}>
-                        Sobre Nosotros
-                    </Typography>
-                    <LinkStyled to="sobre-nosotros">Nuestra historia</LinkStyled>
-                    <LinkStyled to="sobre-nosotros">Misión, visión y valores</LinkStyled>
-                    <LinkStyled to="sobre-nosotros">Carreras</LinkStyled>
-                    <LinkStyled to="aviso-de-privacidad">Politicas de privacidad</LinkStyled>
-                    <LinkStyled to="aviso-de-privacidad">Terminos del servicio</LinkStyled>
-                </Grid>
-                <Grid item xs={12} sm={4} md={3} order={{xs: 3, sm: 5, md: 4}} container direction="column" alignItems="flex-start">
-                    <Typography variant="h6" component="p" sx={{ color: theme.palette.background.paper }}>
-                        Soporte
-                    </Typography>
-                    <LinkStyled to="preguntas-frecuentes">Preguntas frecuentes</LinkStyled>
-                    <LinkStyled to="#">Ayuda en linea</LinkStyled>
-                    <LinkStyled to="#">Confianza y seguridad</LinkStyled>
-                </Grid>
-                {/* LENGUAJE */}
-                <Grid item xs={12} sm={8} md={9} order={{xs: 4, sm: 2, md: 5}}>
-                    <Typography variant="subtitle1" component="p" sx={{ color: theme.palette.background.paper }}>
-                        Idioma
-                    </Typography>
-                    <TextField
+      <Container>
+        <Grid container spacing={2} pb={3}>
+          {/* REDES */}
+          <Grid item xs={12} sm={4} md={3} order={{xs: 6, sm: 1, md: 1}} container alignItems="center">
+            <Grid container direction="column" justifyContent="center" spacing={2}>
+              <Grid item container direction="row" justifyContent={{xs: "center", sm: "flex-start", md: "flex-start"}} wrap='nowrap'>
+                <Link to='https://es-la.facebook.com/' target='_BLANK' aria-label='Ir a la página de Facebook'>
+                  <UseAnimations
+                    animation={facebook}
+                    size={56} 
+                    strokeColor={theme.palette.background.default} 
+                  />
+                </Link>
+                <Link to='https://www.instagram.com/' target='_BLANK' aria-label='Ir a la página de Instagram'>
+                  <UseAnimations
+                    animation={instagram}
+                    size={56} 
+                    strokeColor={theme.palette.background.default} 
+                  />
+                </Link>
+                <Link to='https://youtube.com/' target='_BLANK' aria-label='Ir a la página de YouTube'>
+                  <UseAnimations
+                    animation={youtube}
+                    size={56} 
+                    strokeColor={theme.palette.background.default} 
+                  />
+                </Link>
+              </Grid>
+              <Grid item container justifyContent={{xs: "center", sm: "flex-start", md: "flex-start"}}>
+                <Typography variant="body1" sx={{color: theme.palette.background.paper}}>
+                  &#169; Corazón Huasteco 2023
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* ENLACES */}
+          <Grid item xs={12} sm={4} md={3} order={{xs: 1, sm: 3, md: 2}} container direction="column" alignItems="flex-start">
+            <Typography variant="h6" component="p" sx={{ color: theme.palette.background.paper }}>
+              Categorias
+            </Typography>
+            <LinkStyled to="tematicas/vestimenta">Vestimenta</LinkStyled>
+            <LinkStyled to="tematicas/danza">Danza</LinkStyled>
+            <LinkStyled to="tematicas/gastronomia">Gastronomia</LinkStyled>
+            <LinkStyled to="tematicas/musica">Música</LinkStyled>
+            <LinkStyled to="tematicas/tradiciones">Tradiciones</LinkStyled>
+          </Grid>
+          <Grid item xs={12} sm={4} md={3} order={{xs: 2, sm: 4, md: 3}} container direction="column" alignItems="flex-start">
+            <Typography variant="h6" component="p" sx={{ color: theme.palette.background.paper }}>
+              Sobre Nosotros
+            </Typography>
+            <LinkStyled to="sobre-nosotros">Nuestra historia</LinkStyled>
+            <LinkStyled to="sobre-nosotros">Misión, visión y valores</LinkStyled>
+            <LinkStyled to="sobre-nosotros">Carreras</LinkStyled>
+            <LinkStyled to="aviso-de-privacidad">Politicas de privacidad</LinkStyled>
+            <LinkStyled to="aviso-de-privacidad">Terminos del servicio</LinkStyled>
+          </Grid>
+          <Grid item xs={12} sm={4} md={3} order={{xs: 3, sm: 5, md: 4}} container direction="column" alignItems="flex-start">
+            <Typography variant="h6" component="p" sx={{ color: theme.palette.background.paper }}>
+              Soporte
+            </Typography>
+            <LinkStyled to="preguntas-frecuentes">Preguntas frecuentes</LinkStyled>
+            <LinkStyled to="#">Ayuda en linea</LinkStyled>
+            <LinkStyled to="#">Confianza y seguridad</LinkStyled>
+          </Grid>
+          {/* LENGUAJE */}
+          <Grid item xs={12} sm={8} md={9} order={{xs: 4, sm: 2, md: 5}}>
+            <Typography variant="subtitle1" component="label" htmlFor='language' sx={{ color: theme.palette.background.paper, display: 'block' }}>
+              Selecciona un Idioma:
+            </Typography>
+            <TextField
               onChange={handleClick}
               select
               size="medium"
@@ -159,7 +141,7 @@ export const Footer = () => {
                   backgroundColor: 'red', // Cambia el color de fondo de las opciones
                   color: 'yellow', // Cambia el color del texto de las opciones
                 },
-               }}
+              }}
             >
               {LANGUAGES.map((language) => (
                 <option value={language.code} key={language.code}>
@@ -167,23 +149,22 @@ export const Footer = () => {
                 </option>
               ))}
             </TextField>
-                </Grid>
-                {/* LOGO */}
-                <Grid item xs={12} sm={12} md={3} order={{xs: 5, sm: 6, md: 6}} container direction="row" justifyContent={{xs:"center", sm: "flex-end", md:"flex-start"}}>
-                    <CardMedia
-                        sx={{
-                            // mt: {xs: 10, sm: 5, md: 10}, 
-                            width: 90,
-                            bgcolor: "transparent",
-                        }}
-                        component="img"
-                        height={90}
-                        image={imgLogo1}
-                        alt="icono-huejutla"
-                    />
-                </Grid>
-            </Grid>
-        </Container>
+          </Grid>
+          {/* LOGO */}
+          <Grid item xs={12} sm={12} md={3} order={{xs: 5, sm: 6, md: 6}} container direction="row" justifyContent={{xs:"center", sm: "flex-end", md:"flex-start"}}>
+            <CardMedia
+              sx={{
+                width: 90,
+                bgcolor: "transparent",
+              }}
+              component="img"
+              height={90}
+              image={imgLogo1}
+              alt="icono-huejutla"
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 }
--- src/components/Layout/Footer/footer.elements.jsx
+++ src/components/Layout/Footer/footer.elements.jsx
@@ -2,22 +2,18 @@ import styled   from '@emotion/styled'
 import { IconButton } from '@mui/material'
 import { Link } from 'react-router-dom'
 
-
-
 // contenedor de footer
 export const FooterContent = styled.footer`
   border-top: 1px solid #e1e1e1;
   margin-top: 50rem;
   padding-top: 5rem; 
 `
-//componente router personalizado {link}
 export const LinkStyled = styled(Link)(({ theme }) => ({
-  
   color: "white",
   textDecoration: "none",
 
   '&:hover': {
-      color: theme.palette.background.default,
+    color: theme.palette.background.default,
   }
 }))
 
