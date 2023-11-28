f162eb1876ea32658487c052dbfbd1d441db6d85
Elder-Meyer <meyer.sanchez300@gmail.com>
2023-11-28 09:03:00 -0600

    7.3.2 Deploy Stable Version to Firebase (generated files)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "22271febdc61e0ae248cd93e3ec01f59"
   }, {
     "url": "index.html",
-    "revision": "0.1ged781cmag"
+    "revision": "0.8ebkovn67go"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
