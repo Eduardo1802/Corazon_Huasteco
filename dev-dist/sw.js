7cc12d26df869de48bcb445be55213a3ace9e51c
Elder-Meyer <meyer.sanchez300@gmail.com>
2023-11-29 01:29:09 -0600

    7.3.3 Improve Users Notifications (fnPushNotifications)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "22271febdc61e0ae248cd93e3ec01f59"
   }, {
     "url": "index.html",
-    "revision": "0.8ebkovn67go"
+    "revision": "0.3v86bspqk0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/utils/fnPushNotifications.jsx
+++ src/utils/fnPushNotifications.jsx
@@ -2,11 +2,24 @@ import { getToken } from "firebase/messaging";
 import { messaging } from "../config/firebase/firebaseDB";
 
 export const activarMensajes = async () => {
-  const token = await getToken(messaging, {
-    vapidKey:
-      "BKa4GChbKGeCDVAYDmBnOEp74fbj8L7LIoqcI9rJVDh87GmFni2JRMURakqRNiwYZUzpcwPb8wp-9QQRmOLWJOQ",
-  }).catch((err) => console.log("tuviste un error al generar el token, papá"));
+  let token = null;
 
-  if (token) console.log("token generado: ", token);
-  if (!token) console.log("no se pudo generar el token");
+  // Utiliza un bucle while para realizar verificaciones periódicas
+  while (!token) {
+    // Intenta obtener el token
+    token = await getToken(messaging, {
+      vapidKey:
+        "BKa4GChbKGeCDVAYDmBnOEp74fbj8L7LIoqcI9rJVDh87GmFni2JRMURakqRNiwYZUzpcwPb8wp-9QQRmOLWJOQ",
+    }).catch((err) =>
+      console.log("Tuviste un error al generar el token, papá")
+    );
+
+    // Si el token se generó, imprímelo y sal del bucle
+    if (token) {
+      console.log("Token generado:", token);
+    } else {
+      // Si el token no se generó, espera 2 segundos antes de volver a intentar
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+  }
 };
