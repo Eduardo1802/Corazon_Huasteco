fd31513a674f51544db950e029033f63746772cf
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-01-29 16:43:59 -0600

    7.6.18 Add React HelmetComponent Profiles Views

--- src/views/Profiles/Administrador/Administrador.jsx
+++ src/views/Profiles/Administrador/Administrador.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import VerticalTabs from './VerticalTabs'
+import { HelmetComponent } from '../../../components/customs/HelmetComponent'
 
 export const Administrador = () => {
 
   return (
     <>
+      <HelmetComponent/>
       <VerticalTabs/>
     </>
   )
--- src/views/Profiles/Supervisor/Supervisor.jsx
+++ src/views/Profiles/Supervisor/Supervisor.jsx
@@ -1,9 +1,11 @@
+import { HelmetComponent } from '../../../components/customs/HelmetComponent'
 import VerticalTabs from './VerticalTabs'
 
 export const Supervisor = () => {
 
   return (
     <>
+      <HelmetComponent/>
       <VerticalTabs/>
     </>
   )
